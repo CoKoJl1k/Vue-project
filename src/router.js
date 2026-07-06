3368ae41e064987cf88de5af371aa66be8936664
parfenovich <mlz.psant@gmail.com>
2026-07-06 10:22:35 +0300

    Fix:  front build

--- src/router.js
+++ src/router.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import NotesPage from './pages/NotesPage.vue'
 import HomePage from './pages/HomePage.vue'
 import CurrencyPage from "@/pages/CurrencyPage.vue";
@@ -10,6 +10,6 @@ const routes = [
 ]
 
 export default createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 })
